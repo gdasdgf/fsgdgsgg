31a4878a604be075df5963cb73244326fbc4a0f7
gdasdgf <133189975+gdasdgf@users.noreply.github.com>
2023-05-15 22:21:41 +0600

    Add files via upload

---
+++ link.js
@@ -0,0 +1,7 @@
+
+const modelName = "jaccisa"
+
+const link = "
+https://m.youtube.com/redirect?event=comments&redir_token=QUFFLUhqa1JFVWJna3FWUURwWlZ0MVFndERWalk4YUdlQXxBQ3Jtc0trV3IzOHdRSFNfQUZzbWZONTlBX252d1ZEaFlnRk9xYnZBbEl3d0tuTXVLTE1OU0RoSGpMS0ZjbHM0Wl95c3d3TG1tMXpfX3pRVnhVbno2T1RWMkJnRmx4b2ptcG9xRGhmRjNDTXl0eGkzRkhYMmgxNA&q=https%3A%2F%2Ftinyurl.com%2Fycxzzz8v&html_redirect=1
+
+"
